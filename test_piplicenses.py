0166aba8ffb72756a1e90b6b46f2f361009c5216
raimon <raimon49@hotmail.com>
2019-07-29 22:31:45 +0900

    Set magic comment and Vim modeline [ci skip]

--- test_piplicenses.py
+++ test_piplicenses.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
 from __future__ import (division, print_function,
                         absolute_import, unicode_literals)
 import unittest
