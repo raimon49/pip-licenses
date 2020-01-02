80c20bdcc43c2dcb7853e1f497fd3ac4be73b983
raimon <raimon49@hotmail.com>
2020-01-02 22:52:47 +0900

    Remove import statement from '__future__'

--- piplicenses.py
+++ piplicenses.py
@@ -26,8 +26,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from __future__ import (division, print_function,
-                        absolute_import, unicode_literals)
 import sys
 import glob
 import os
