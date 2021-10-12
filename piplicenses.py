2c93e3a3d92a4c4b171a2c9ff365fe6d59cf532b
raimon <raimon49@hotmail.com>
2021-10-12 18:58:47 +0900

    Bump version to 3.5.3

--- piplicenses.py
+++ piplicenses.py
@@ -81,7 +81,7 @@ except ImportError:  # pragma: no cover
 open = open  # allow monkey patching
 
 __pkgname__ = 'pip-licenses'
-__version__ = '3.5.2'
+__version__ = '3.5.3'
 __author__ = 'raimon'
 __license__ = 'MIT'
 __summary__ = ('Dump the software license list of '
