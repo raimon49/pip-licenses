1b9bc1b3a71daeb4d51345aec75b459a4ccdb6e6
raimon <raimon49@hotmail.com>
2023-04-25 17:08:35 +0900

    Bump version to 4.3.0

--- piplicenses.py
+++ piplicenses.py
@@ -56,7 +56,7 @@ if TYPE_CHECKING:
 open = open  # allow monkey patching
 
 __pkgname__ = "pip-licenses"
-__version__ = "4.2.0"
+__version__ = "4.3.0"
 __author__ = "raimon"
 __license__ = "MIT"
 __summary__ = (
