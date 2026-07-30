d883bb70678ae5879d9bf53af4628dc4261137f8
Stefan <96178532+stefan6419846@users.noreply.github.com>
2026-07-29 17:19:17 -0700

    [FILTER] Pin relevant files with fixes from 0b1c2ae56d0d8bd0bac078bf758cc770da5527b0 ( From stefan6419846/pip-licenses-cli#132 ) for ensuring proper credit is given to Stefan for their code Copyright 2025 Stefan (under MIT License)
    On derived branch trampoline-from-pip-licenses-cli-PR-132
    Changes to be committed:
    	deleted:    .flake8
    	deleted:    .github/dependabot.yml
    	deleted:    .github/workflows/ci.yml
    	deleted:    .github/workflows/release.yml
    	deleted:    .gitignore
    	deleted:    CHANGELOG.md
    	deleted:    CONTRIBUTING.md
    	deleted:    Dockerfile
    	deleted:    MANIFEST.in
    	deleted:    Makefile
    	deleted:    README.md
    	deleted:    USAGE.md
    	deleted:    docker/.gitkeep
    	deleted:    piplicenses/__init__.py
    	deleted:    piplicenses/__main__.py
    	deleted:    piplicenses/collector.py
    	deleted:    piplicenses/errors.py
    	deleted:    piplicenses/py.typed
    	deleted:    piplicenses/spdx.py
    	deleted:    pyproject.toml
    	deleted:    requirements/dev.txt
    	deleted:    requirements/main.txt
    	deleted:    stubs/boolean.pyi
    	deleted:    stubs/license_expression.pyi
    	deleted:    tests/__init__.py
    	deleted:    tests/fixtures/unicode_characters.txt
    	deleted:    tests/test_collector.py
    	deleted:    tests/test_integration.py
    	deleted:    tests/test_output.py
    	deleted:    tests/test_spdx.py
    
    Co-Authored-by: Stefan <96178532+stefan6419846@users.noreply.github.com>
    Co-Authored-by: Mr. Walls  <reactive-firewall@users.noreply.github.com>

--- piplicenses/__main__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: Copyright (c) 2018 raimon
-# SPDX-FileCopyrightText: Copyright (c) 2025 stefan6419846
-
-if __name__ == "__main__":  # pragma: no cover
-    from piplicenses.cli import main
-
-    main()
--- piplicenses/errors.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: Copyright (c) 2018 raimon
-# SPDX-FileCopyrightText: Copyright (c) 2025 stefan6419846
-
-
-class PipLicensesWarning(UserWarning):
-    """
-    Base class for warnings emitted by the pip-licenses-cli package.
-    """
