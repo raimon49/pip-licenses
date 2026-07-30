54da0fb92fea92560b3cd9e3687ee12af1103116
Mr. Walls <reactive-firewall@users.noreply.github.com>
2026-07-29 22:45:35 -0700

    Release Candidate for v6.0 alpha (EXPEREMENTAL) Dev Build

--- piplicenses.py
+++ piplicenses.py
@@ -59,7 +59,7 @@ if TYPE_CHECKING:  # pragma: no cover
 open = open  # allow monkey patching
 
 __pkgname__ = "pip-licenses"
-__version__ = "6.0.0"  # (dev-v6.0 branch)
+__version__ = "6.0.0a0"  # (dev-v6.0 branch)
 __summary__ = (
     "Dump the software license list of Python packages installed with pip."
 )
