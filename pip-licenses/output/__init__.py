ee5e354810b4b54c299a99482445265167c4ef9b
Mr. Walls <reactive-firewall@users.noreply.github.com>
2026-08-04 18:22:26 -0700

    Split off just outputs for GHI #316
    all output formats

---
+++ pip-licenses/output/__init__.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et
+"""
+pip-licenses.output
+
+MIT License
+
+Copyright (c) 2018-2025 raimon
+Copyright (c) 2025-2026 Mr. Walls
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from .. import (
+    __pkgname__,
+    __version__,
+)
+
+
+from prettytable import (
+    HRuleStyle,
+    PrettyTable,
+    RowType,
+)
+
+
+class JsonPrettyTable(PrettyTable):
+    """PrettyTable-like class exporting to JSON"""
+
+    def format_row(self, row: RowType) -> dict[str, str | list[str]]:
+        return dict(zip(self._field_names, row))
+
+    def get_string(self, **kwargs: str | list[str]) -> str:
+        # import included here in order to limit dependencies
+        # if not interested in JSON output,
+        # then the dependency is not required
+        import json
+
+        options = self._get_options(kwargs)
+        rows = self._get_rows(options)
+        lines = [self.format_row(row) for row in rows]
+        return json.dumps(lines, indent=2, sort_keys=True)
+
+
+class JsonLicenseFinderTable(JsonPrettyTable):
+    def format_row(self, row: RowType) -> dict[str, str | list[str]]:
+        resrow: dict[str, str | list[str]] = {}
+        for field, value in zip(self._field_names, row):
+            if field == "Name":
+                resrow["name"] = value
+
+            if field == "Version":
+                resrow["version"] = value
+
+            if field == "License":
+                resrow["licenses"] = [value]
+
+        return resrow
+
+    def get_string(self, **kwargs: str | list[str]) -> str:
+        # import included here in order to limit dependencies
+        # if not interested in JSON output,
+        # then the dependency is not required
+        import json
+
+        options = self._get_options(kwargs)
+        rows = self._get_rows(options)
+        lines = [self.format_row(row) for row in rows]
+        return json.dumps(lines, sort_keys=True)
+
+
+class CSVPrettyTable(PrettyTable):
+    """PrettyTable-like class exporting to CSV"""
+
+    def get_string(self, **kwargs: str | list[str]) -> str:
+        def esc_quotes(val: bytes | str) -> str:
+            """
+            Meta-escaping double quotes
+            https://tools.ietf.org/html/rfc4180
+            """
+            try:
+                return cast(str, val).replace('"', '""')
+            except UnicodeDecodeError:  # pragma: no cover
+                return cast(bytes, val).decode("utf-8").replace('"', '""')
+            except UnicodeEncodeError:  # pragma: no cover
+                return str(
+                    cast(str, val).encode("unicode_escape").replace('"', '""')  # type: ignore[arg-type]
+                )
+
+        options = self._get_options(kwargs)
+        rows = self._get_rows(options)
+        formatted_rows = self._format_rows(rows)
+
+        lines: list[str] = []
+        formatted_header = ",".join(
+            [f'"{esc_quotes(val)}"' for val in self._field_names]
+        )
+        lines.append(formatted_header)
+        lines.extend(
+            [
+                ",".join([f'"{esc_quotes(val)}"' for val in row])
+                for row in formatted_rows
+            ]
+        )
+
+        return "\n".join(lines)
+
+
+class PlainVerticalTable(PrettyTable):
+    """PrettyTable for outputting to a simple non-column based style.
+
+    When used with --with-license-file, this style is similar to the default
+    style generated from Angular CLI's --extractLicenses flag.
+    """
+
+    def get_string(self, **kwargs: str | list[str]) -> str:
+        options = self._get_options(kwargs)
+        rows = self._get_rows(options)
+
+        output = ""
+        for row in rows:
+            for v in row:
+                output += f"{v}\n"
+            output += "\n"
+
+        return output
+
+
+def factory_styled_table_with_args(
+    args: CustomNamespace,
+    output_fields: set[str] | Sequence[str] = DEFAULT_OUTPUT_FIELDS,
+) -> PrettyTable:
+    table = PrettyTable()
+    table.field_names = output_fields  # type: ignore[assignment]
+    table.align = "l"  # type: ignore[assignment]
+    table.border = args.format_ in (
+        FormatArg.MARKDOWN,
+        FormatArg.RST,
+        FormatArg.CONFLUENCE,
+        FormatArg.JSON,
+    )
+    table.header = True
+
+    if args.format_ == FormatArg.MARKDOWN:
+        table.junction_char = "|"
+        table.hrules = HRuleStyle.HEADER
+    elif args.format_ == FormatArg.RST:
+        table.junction_char = "+"
+        table.hrules = HRuleStyle.ALL
+    elif args.format_ == FormatArg.CONFLUENCE:
+        table.junction_char = "|"
+        table.hrules = HRuleStyle.NONE
+    elif args.format_ == FormatArg.JSON:
+        table = JsonPrettyTable(table.field_names)
+    elif args.format_ == FormatArg.JSON_LICENSE_FINDER:
+        table = JsonLicenseFinderTable(table.field_names)
+    elif args.format_ == FormatArg.CSV:
+        table = CSVPrettyTable(table.field_names)
+    elif args.format_ == FormatArg.PLAIN_VERTICAL:
+        table = PlainVerticalTable(table.field_names)
+
+    return table
+
+
+def get_output_fields(args: CustomNamespace) -> list[str]:
+    if args.summary:
+        return list(SUMMARY_OUTPUT_FIELDS)
+
+    output_fields = list(DEFAULT_OUTPUT_FIELDS)
+
+    if args.from_ == FromArg.ALL:
+        output_fields.append("License-Metadata")
+        output_fields.append("License-Classifier")
+        output_fields.append("License-Expression")
+    else:
+        output_fields.append("License")
+
+    if args.with_authors:
+        output_fields.append("Author")
+
+    if args.with_maintainers:
+        output_fields.append("Maintainer")
+
+    if args.with_urls:
+        output_fields.append("URL")
+
+    if args.with_description:
+        output_fields.append("Description")
+
+    if args.no_version:
+        output_fields.remove("Version")
+
+    if args.with_license_file:
+        if not args.no_license_path:
+            output_fields.append("LicenseFile")
+
+        output_fields.append("LicenseText")
+
+        if args.with_notice_file:
+            output_fields.append("NoticeText")
+            if not args.no_license_path:
+                output_fields.append("NoticeFile")
+
+    return output_fields
+
+
+def create_output_string(args: CustomNamespace) -> str:
+    output_fields = get_output_fields(args)
+
+    if args.summary:
+        table = create_summary_table(args)
+    else:
+        table = create_licenses_table(args, output_fields)
+
+    sortby = get_sortby(args)
+
+    if args.format_ == FormatArg.HTML:
+        html = table.get_html_string(fields=output_fields, sortby=sortby)
+        return html.encode("ascii", errors="xmlcharrefreplace").decode("ascii")
+    else:
+        return table.get_string(fields=output_fields, sortby=sortby)
+
